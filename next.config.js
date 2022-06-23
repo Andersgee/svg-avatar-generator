6e4696bbf8ef748371db1aa0084696f44bb12085
Anders Gustafsson <andersgee@gmail.com>
2022-06-23 20:07:48 +0200

    simple svg import

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
+const svgrconfig = {
+  test: /\.svg$/i,
+  issuer: { and: [/\.(js|ts|md)x?$/] },
+  use: [
+    {
+      loader: "@svgr/webpack",
+      options: {
+        //https://react-svgr.com/docs/options/
+        svgo: true, //use svg Optimizer
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                //https://github.com/svg/svgo#default-preset
+                //use preset-default but with some overrides
+                overrides: {
+                  cleanupIDs: false,
+                  prefixIds: false,
+                  removeTitle: false,
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(svgrconfig);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
